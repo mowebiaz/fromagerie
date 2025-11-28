70fbb05ace28ffcf948093ae09ef698e951c7eb6
mowebiaz <mowebiaz@gmail.com>
2025-11-28 09:52:19 +0100

    Modify footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
-'use client'
-
+import { FaInstagramSquare } from 'react-icons/fa'
+import { FaFacebook, FaGithub } from 'react-icons/fa6'
+import { LinkAria } from '@/src/components/LinkAria/LinkAria'
 import './Footer.scss'
 
 export function Footer() {
@@ -8,36 +9,55 @@ export function Footer() {
       <div className="footer__top">
         {/* logo */}
         <div className="footer__info">
-          <p>adress
+          <p className="footer__title">Coordonnées</p>
+          <p>
+            xxxxx
             <br />
-            adress
+            74110 Avoriaz
           </p>
-          <p>telephone</p>
-          <p>email</p>
+          <p>04 50 74 00 00</p>
+          <p>fromagerie-cimes@example.com</p>
         </div>
         <div className="footer__hours">
-          <h3 className='footer__title'>Horaires d'ouverture</h3>
-          <p>Lundi : Fermé</p>
-          <p>Mardi - Vendredi : 10h - 19h</p>
-          <p>Samedi : 10h - 20h</p>
-          <p>Dimanche : 9h - 13h</p>
+          <p className="footer__title">Horaires d'ouverture</p>
+          <p>En saison:</p>
+          <p>Tous les jours</p>
+          <p>7h30 - 13h</p>
+          <p>16h - 19h</p>
         </div>
         <div className="footer__social">
-          <h3 className='footer__title'>Suivez-nous</h3>
+          <p className="footer__title">Suivez-nous</p>
           <div className="footer__social-links">
-            <a href="#" className="footer__social-link">Facebook</a>
-            <a href="#" className="footer__social-link">Instagram</a>
+            <LinkAria
+              href={'/'}
+              target="_blank"
+              referrerPolicy="no-opener no-referrer"
+              aria-label="Lien vers le profil instagram"
+              className="social-link"
+            >
+              <FaInstagramSquare />
+            </LinkAria>
+
+            <LinkAria
+              href={'/'}
+              target="_blank"
+              referrerPolicy="no-opener no-referrer"
+              aria-label="Lien vers le profil Facebook"
+              className="social-link"
+            >
+              <FaFacebook />
+            </LinkAria>
           </div>
         </div>
       </div>
       <div className="footer__bottom">
-        <div className='footer__copyright'>
-        <p>© 2025 Fromagerie des Cimes. Tous droits réservés.</p>
-        <p>Site réalisé par MorganeWeb</p>
+        <div className="footer__copyright">
+          <p>© 2025 Fromagerie des Cimes. Tous droits réservés.</p>
+          <p>Site réalisé par MorganeWeb</p>
         </div>
         <div className="footer__links">
-          <a href="#">Mentions légales</a>
-          <a href="#">Politique de confidentialité</a>
+          <LinkAria href={'/'} className="link">Mentions légales</LinkAria>
+          <LinkAria href={'/'} className="link">Politique de confidentialité</LinkAria>
         </div>
       </div>
     </footer>
